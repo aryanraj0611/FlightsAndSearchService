33d42892f44efa8b7e0f0b02e20a26c662b067a8
aryanraj0611 <aryanraj01010@gmail.com>
2026-03-20 19:06:37 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,7 +4,9 @@ class CityRepository{
 
     async createCity({name}){
         try {
-            const city= await City.create({name});
+            const city= await City.create({
+                name
+            });
             return city;
         }
         catch(error){
@@ -27,6 +29,34 @@ class CityRepository{
             throw error;
         }
     }
+
+    async updateCity({cityId, data}){
+        try {
+            const city= await City.update(data,{
+                where:{
+                    id: cityId
+                }
+            })
+            return true;
+        }
+        catch(error){
+            console.log("Something went wrong in repository layer");
+            throw error;
+        } 
+    }
+
+    async getCity(cityId){
+        try {
+            const city= await City.findByPk(cityId);
+
+            return city;
+        }
+        catch(error){
+            console.log("Something went wrong in repository layer");
+            throw error;
+        }
+    }
+
 }
 
 module.exports=CityRepository; 

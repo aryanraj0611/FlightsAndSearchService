405500b248586bb4f909fb70293164ae3e7b197f
aryanraj0611 <aryanraj01010@gmail.com>
2026-03-28 20:01:55 +0530

    added airport seeder

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ const bodyParser= require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const ApiRoutes= require('./routes/index');  
 
-const setupAndStartServer = async()=>{
+const db = require('./models/index');
+const {City, Airport} = require('./models/index');
+const sequelize = require('sequelize');
 
+const setupAndStartServer = async()=>{
+ 
     //create an express object
     const app = express();
 
@@ -13,9 +17,11 @@ const setupAndStartServer = async()=>{
     app.use(bodyParser.urlencoded({extended: true}));
     app.use('/api', ApiRoutes); 
 
-    app.listen(PORT, ()=>{
+    app.listen(PORT, async ()=>{
         console.log(`Server started at ${PORT}`);   
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter: true});
+        }
     })
 }
-
 setupAndStartServer(); 

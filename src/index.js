36fab98bb52335a9f2458d76e37206b655006d4c
aryanraj0611 <aryanraj01010@gmail.com>
2026-03-28 23:43:58 +0530

    added airplanes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const {PORT} = require('./config/serverConfig');
 const ApiRoutes= require('./routes/index');  
 
 const db = require('./models/index');
-const {City, Airport} = require('./models/index');
-const sequelize = require('sequelize');
+// const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async()=>{
  
@@ -22,6 +21,7 @@ const setupAndStartServer = async()=>{
         if(process.env.SYNC_DB){
             db.sequelize.sync({alter: true});
         }
-    })
+        
+    });
 }
 setupAndStartServer(); 
